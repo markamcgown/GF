41fc6cbf76ceceaf6abbf218091e8392e06b9e5b
markamcgown <42589478+markamcgown@users.noreply.github.com>
2020-12-21 08:51:34 -0500

    Add files via upload

--- VetData_SF_ETL/main.py
+++ VetData_SF_ETL/main.py
@@ -8,7 +8,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/task/loader')
 def sfLibraries():
     url = r'https://api.vetdata.net/InstallationList'
     user = 'futurepet'
