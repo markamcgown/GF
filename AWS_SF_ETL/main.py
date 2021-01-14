87c63b32c4a7ce9b3676d284f21efd8841f02e42
markamcgown <42589478+markamcgown@users.noreply.github.com>
2021-01-13 18:55:31 -0800

    Add files via upload

--- AWS_SF_ETL/main.py
+++ AWS_SF_ETL/main.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 @app.route('/task/aws_loader')
 def load_aws():
   env = 'PROD'#
-  tables = ['Promotions','User_Checklist_Group_Records','User_Checklist_Item_Records','Pet_Checklist_Group_Records','Pet_Checklist_Item_Records','Claims','Rewards','Withdrawals','User_Promotions','Users','External_User_Identifiers']
-  sub_col = ['DEACTIVATED_AT','OCCURRED_ON','EXPIRY','START_DATE','END_DATE','DELETED_AT']
+  tables = ['Pets','Promotions','User_Checklist_Group_Records','User_Checklist_Item_Records','Pet_Checklist_Group_Records','Pet_Checklist_Item_Records','Claims','Rewards','Withdrawals','User_Promotions','Users','External_User_Identifiers']
+  sub_col = ['DEACTIVATED_AT','OCCURRED_ON','EXPIRY','START_DATE','END_DATE','DATE_OF_BIRTH','DELETED_AT']
 
   for table in tables:
     columns = []
